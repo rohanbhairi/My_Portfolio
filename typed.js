c8a4b3119b99594ef1267960f4a4bc4c1d03a9c5
rohan <rohanbhairi1210@gmail.com>
2024-12-26 22:58:33 +0530

    modified 1.0.0

--- typed.js
+++ typed.js
@@ -18,6 +18,16 @@ window.addEventListener("scroll", ()=>{
     // console.log(nav);
     nav.classList.toggle("sticky", window.scrollY > 0);
     console.log(nav);
-    
-    
 })
+
+var menuBtn = document.getElementsByClassName("bx-menu")[0];
+console.log(menuBtn);
+
+if (menuBtn) {
+    menuBtn.addEventListener("click", () => {
+        console.log("clicked");
+    });
+} else {
+    console.log("menuBtn not found");
+}
+
